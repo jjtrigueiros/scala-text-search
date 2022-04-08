dda4e29cd20f20b5a15b103952a808d4b23e8d22
J. Trigueiros <jjtrigueiros@gmail.com>
2022-04-08 16:49:40 +0100

    Implement index as map of files and contents
    misc: update tests, add testing example folder

--- src/main/scala/textsearch/SearchMain.scala
+++ src/main/scala/textsearch/SearchMain.scala
@@ -1,7 +1,9 @@
 package textsearch
 
 import java.io.File
-import scala.util.Try
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.{Failure, Success, Try, Using}
 
 object SearchMain extends App {
   Program
@@ -15,7 +17,9 @@ object SearchMain extends App {
 object Program {
   import scala.io.StdIn.readLine
 
-  case class Index() // TODO: Implement this
+  case class Index(filesAndContents: Map[File, Try[String]])
+  // Class representing files indexed in memory.
+  // A map of (file -> contents) value pairs.
 
   sealed trait ReadDirectoryError
 
@@ -36,8 +40,26 @@ object Program {
   }
 
   // TODO: Index all files in the directory
-  def index(directory: File): Index = ???
+  def index(directory: File): Index = {
+    def readFile(file: File): Try[String] = {
+      Using(Source.fromFile(file)){
+        open_file => open_file.getLines().reduceLeft(_+_)
+      }
+    }
 
+    val files = directory.listFiles.filter { f => f.isFile && f.getName.endsWith(".txt") }.toList
+
+    val files_and_contents = (files zip files.map(f => readFile(f))).toMap
+
+    // TODO: Alert the user if a text file could not be opened
+    // TODO: Try validating below with fold (check signature)
+    // val valid_files_and_contents =
+    //   files_and_contents.map { case (file: File, Success(content)) => (file, content) }
+
+    Index(files_and_contents)
+  }
+
+  @tailrec
   def iterate(indexedFiles: Index): Unit = {
     print(s"search> ")
     val searchString = readLine()
--- src/test/scala/textsearch/SearchMainSuite.scala
+++ src/test/scala/textsearch/SearchMainSuite.scala
@@ -1,9 +1,10 @@
 package textsearch
 
 import munit.FunSuite
+
 import java.io.File
-//import scala.io.Source
-import scala.util.Try
+import scala.io.{BufferedSource, Source}
+import scala.util.{Try, Using}
 
 
 class SearchMainSuite extends FunSuite {
@@ -21,6 +22,29 @@ class SearchMainSuite extends FunSuite {
     val test_args: Array[String] = Array(path)
 
     val obtained = Program.readDirectory(test_args).fold(println, dir => Program.index(dir))
-    assert(obtained.isInstanceOf[Program.Index])
+    obtained match{
+      case obtained: Program.Index =>
+        assert(obtained.filesAndContents.sizeIs == 28)
+      case _ =>
+        fail("Obtained something other than a Program.Index")
+    }
   }
+
+  // test("Ignores an open file"){
+  //   val filepath: String = getClass.getResource("/test_folder/203.txt").getPath
+  //   Using(Source.fromFile(filepath)){
+  //     open_file =>
+  //       val path: String = getClass.getResource("/test_folder").getPath
+  //       val test_args: Array[String] = Array(path)
+  //       val obtained = Program.readDirectory(test_args).fold(println, dir => Program.index(dir))
+  //       obtained match{
+  //         case obtained: Program.Index =>
+  //           assert(obtained.filesAndContents.sizeIs == 9999) // not working
+  //         case _ =>
+  //           assert(false)
+  //       }
+  //   }
+  // }
+
+
 }
