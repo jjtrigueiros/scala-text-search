f571adae0b8889af9cfab0300c581976083e5930
J. Trigueiros <jjtrigueiros@gmail.com>
2022-04-10 22:57:55 +0100

    Implement core functionality

--- src/main/scala/textsearch/SearchMain.scala
+++ src/main/scala/textsearch/SearchMain.scala
@@ -4,22 +4,27 @@ import java.io.File
 import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.{Failure, Success, Try, Using}
+import scala.util.matching.Regex
 
 object SearchMain extends App {
   Program
     .readDirectory(args)
     .fold(
       println,
-      dir => Program.iterate(Program.index(dir))
+      dir => Program.index(dir)
+        .fold(
+          println,
+          index => Program.iterate(index)
+        )
     )
 }
 
 object Program {
   import scala.io.StdIn.readLine
 
-  case class Index(filesAndContents: Map[File, Try[String]])
+  case class Index(filesAndWords: List[(File, Map[String, Int])])
   // Class representing files indexed in memory.
-  // A map of (file -> contents) value pairs.
+  // A list of (file, map(words -> n_matches)) value pairs.
 
   sealed trait ReadDirectoryError
 
@@ -39,31 +44,77 @@ object Program {
     } yield directory
   }
 
-  // TODO: Index all files in the directory
-  def index(directory: File): Index = {
+  def parseWords(src: String): Map[String, Int] = { // : Map[String, Int]
+    // From a provided string, returns a map of (found word -> number of times word was found)
+    // Word defined as a case-insensitive sequence of letters
+    // which can be interrupted by one consecutive hyphen or apostrophe
+    // Ex.: banana, should've, state-of-the-art
+    val wordPattern: Regex = "[^\\W\\d]([A-Za-z]|[-'](?=[A-Za-z]))*".r
+    wordPattern.findAllIn(src)
+      .toList.map(word => word.toLowerCase)
+      .groupBy(identity)
+      .transform((_, v) => v.size)
+  }
+
+  def index(directory: File): Try[Index] = {
     def readFile(file: File): Try[String] = {
       Using(Source.fromFile(file)){
         open_file => open_file.getLines().reduceLeft(_+_)
       }
     }
 
+    // filter for valid .txt files
     val files = directory.listFiles.filter { f => f.isFile && f.getName.endsWith(".txt") }.toList
+    // attempt to open each file, then filter out files that could not be opened
+    val listOfTries: List[(File, Try[String])] = files zip files.map(f => readFile(f))
+    // TODO: Alert user if a .txt file could not be read
+    val tryFilesAndContents: Try[List[(File, String)]] = {
+      Try(
+        listOfTries.filter(_._2.isSuccess)
+          .map(fileAndContents => (fileAndContents._1, fileAndContents._2.get))
+      )
+    }
 
-    val files_and_contents = (files zip files.map(f => readFile(f))).toMap
-
-    // TODO: Alert the user if a text file could not be opened
-    // TODO: Try validating below with fold (check signature)
-    // val valid_files_and_contents =
-    //   files_and_contents.map { case (file: File, Success(content)) => (file, content) }
-
-    Index(files_and_contents)
+    // convert Try[List[(File, String)]] to Try[Index] if successfully filtered
+    tryFilesAndContents match {
+      case Success(contents) => Success(
+        Index(contents.map(fileAndContents => (fileAndContents._1, parseWords(fileAndContents._2))))
+      )
+      case Failure(exception) => Failure(exception)
+    }
   }
 
   @tailrec
   def iterate(indexedFiles: Index): Unit = {
+    // For a given map of words and their occurrences,
+    // returns the number of occurrences of the provided searchWord up to a maximum of maxHits
+    def scoreWord(wordCounts: Map[String, Int], searchWord: String, maxHits: Int): Int ={
+      wordCounts.get(searchWord) match {
+        case Some(nCount) => maxHits min nCount
+        case None => 0
+      }
+    }
+
+    // For a given indexed file, tallies the score for the list of searchWords
+    // Returns a tuple of filename and percentage of words matched.
+    def scoreFile(indexedFile: (File, Map[String, Int]), searchWords: Map[String, Int]): (String, Float) ={
+      val maxScore: Int = searchWords.values.sum
+      val filename = indexedFile._1.getName
+      val score: Int = searchWords.foldLeft(0)((acc, next) => acc + scoreWord(indexedFile._2, next._1, next._2))
+      val rating: Float = (score.toFloat/maxScore.toFloat)*100
+      (filename, rating)
+    }
+
     print(s"search> ")
     val searchString = readLine()
     // TODO: Make it print the ranking of each file and its corresponding score
+    val searchWords = parseWords(searchString)
+    val ranking: List[(String, Float)] =
+      indexedFiles.filesAndWords.map(scoreFile(_, searchWords))
+        .sortBy(_._2).reverse
+    for((file, rank) <- ranking.take(10)){
+      println("["+rank+"%]: "+file)
+    }
     iterate(indexedFiles)
   }
 }
--- src/test/scala/textsearch/SearchMainSuite.scala
+++ src/test/scala/textsearch/SearchMainSuite.scala
@@ -4,7 +4,7 @@ import munit.FunSuite
 
 import java.io.File
 import scala.io.{BufferedSource, Source}
-import scala.util.{Try, Using}
+import scala.util.{Success, Try, Using}
 
 
 class SearchMainSuite extends FunSuite {
@@ -21,30 +21,50 @@ class SearchMainSuite extends FunSuite {
     val path: String = getClass.getResource("/test_folder").getPath
     val test_args: Array[String] = Array(path)
 
-    val obtained = Program.readDirectory(test_args).fold(println, dir => Program.index(dir))
+    val obtained = Program.readDirectory(test_args)
+      .fold(println, dir => Program.index(dir)
+        .fold(println, index => index))
+
     obtained match{
-      case obtained: Program.Index =>
-        assert(obtained.filesAndContents.sizeIs == 28)
+      case index: Program.Index =>
+        assert(index.filesAndWords.sizeIs == 28)
       case _ =>
-        fail("Obtained something other than a Program.Index")
+        fail("Failed to generate a Program.Index")
     }
   }
 
-  // test("Ignores an open file"){
-  //   val filepath: String = getClass.getResource("/test_folder/203.txt").getPath
-  //   Using(Source.fromFile(filepath)){
-  //     open_file =>
-  //       val path: String = getClass.getResource("/test_folder").getPath
-  //       val test_args: Array[String] = Array(path)
-  //       val obtained = Program.readDirectory(test_args).fold(println, dir => Program.index(dir))
-  //       obtained match{
-  //         case obtained: Program.Index =>
-  //           assert(obtained.filesAndContents.sizeIs == 9999) // not working
-  //         case _ =>
-  //           assert(false)
-  //       }
-  //   }
-  // }
+  //test("Ignores an open file"){
+  //  val filepath: String = getClass.getResource("/test_folder/203.txt").getPath
+  //  Using(Source.fromFile(filepath)){
+  //    open_file =>
+  //      val path: String = getClass.getResource("/test_folder").getPath
+  //      val test_args: Array[String] = Array(path)
+  //      val obtained = Program.readDirectory(test_args).fold(println, dir => Program.index(dir))
+  //      obtained match{
+  //        case obtained: Program.Index =>
+  //          assert(obtained.filesAndWords.sizeIs == 9999) // not working
+  //        case _ =>
+  //          fail("Obtained something other than a Program.Index")
+  //      }
+  //  }
+  //}
 
+  // test match simple word
+
+  test("Parse example words") {
+    val obtained = Program.parseWords("Trader's state-of-the-art State states States are Stoats")
+    assert(obtained.contains("state"))
+    assert(obtained.contains("trader's"))
+    assert(obtained.contains("state-of-the-art"))
+  }
+
+  // test match word with apostrophe (Trader's Cove should have a match in 203.txt)
+
+  // regex: "[^\W\d](\w|[-']{1,2}(?=\w))*"
+  // regex: "[^\W\d]([A-Za-z]|[-'](?=[A-Za-z]))*"
+  //    => matches a sequence of letters interrupted by, at most, one ' or -
+  //    (trader's state-of-the-art)
+  // (y'all'd've is a contrived, yet valid word)
+  // TODO: document what a word is in the README
 
 }
